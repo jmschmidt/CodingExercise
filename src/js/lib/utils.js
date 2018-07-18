9c3cfee08e3c0d8179ca6e36141d1809734d2f75
Joseph Schmidt <joe@josephmschmidt.com>
2018-07-18 10:26:19 -0700

    Fix typo when getting yesterdays date

--- src/js/lib/utils.js
+++ src/js/lib/utils.js
@@ -2,6 +2,12 @@
  * Fetch data from mlb api for yesterday.
  * @param {Function} callback 
  */
+
+/*
+ *  TODO: Previous date does not always return games data
+ *  Possible alternative is to keep fetching the next previous day
+ *  until return games array is not null
+ */
 export function fetchData(callback) {
   // API Url for recap data for yesterdays games. - Using yesterdays date instead of todays
   // API does not always returning a recap object for todays games
@@ -23,7 +29,7 @@ export function fetchData(callback) {
  */
 function getYesterdaysDate() {
   const d = new Date();
-  d.setDate(d.getDate() - 2);
+  d.setDate(d.getDate() - 1);
   
   return d.toISOString().substring(0, 10);
 }
